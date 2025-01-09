9b558af2f5d7b5ace434af4f425d8e7b7edd3bf2
Basti <cimerawow@gmail.com>
2025-01-09 23:04:12 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+    import {
+        getAuth,
+        createUserWithEmailAndPassword,
+        signInWithEmailAndPassword,
+        onAuthStateChanged,
+        signOut
+    } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
